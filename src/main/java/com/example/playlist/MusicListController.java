c1d86f1120e7f91b7537121073318f0545bf47af
Ixor <stulnikovan1337@gmail.com>
2022-12-15 19:01:17 +0300

    second window design and controller

---
+++ src/main/java/com/example/playlist/MusicListController.java
@@ -0,0 +1,21 @@
+package com.example.playlist;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class MusicListController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.playlist {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
 
     opens com.example.playlist to javafx.fxml;
